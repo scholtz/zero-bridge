1387cd103b345a3f45d48af07e298862d2d57bf1
Scholtz <ludko2@gmail.com>
2021-12-05 09:10:13 +0100

    regtest works

--- docker/context-regtest/init.sh
+++ docker/context-regtest/init.sh
@@ -1,4 +1,11 @@
+sed -i s/#rpcallowip=0.0.0.0/rpcallowip=0.0.0.0/g 1/bitcoin.conf
+sed -i s/#rpcallowip=0.0.0.0/rpcallowip=0.0.0.0/g 2/bitcoin.conf
+
+echo "rpcbind=0.0.0.0:19001" >> 1/bitcoin.conf
+echo "rpcbind=0.0.0.0:19011" >> 2/bitcoin.conf
+
 make start
+sleep 1
 #bitcoin-cli -datadir=1 createwallet wallet1
 #bitcoin-cli -datadir=2 createwallet wallet2
 bitcoin-cli -datadir=1 loadwallet wallet1
